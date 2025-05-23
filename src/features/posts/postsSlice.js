051549b2bd23500e27394b1cd9dcd46533f4a052
sayembillah <sayembillah2000@gmail.com>
2025-05-23 18:58:59 +0600

    Completed Slice and imported to store

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,9 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [{}, {}];
+const initialState = [
+  { id: "1", title: "Learning RTk", content: "I have heard good things" },
+  {
+    id: "2",
+    title: "Learning Slices",
+    content: "I have heard good things as well",
+  },
+];
 
 const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {},
 });
+
+export default postsSlice.reducer;
